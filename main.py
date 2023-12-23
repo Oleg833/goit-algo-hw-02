b673c786119d48c1c7e5430201095cd7943ea833
Oleg833 <oleg.kovtun447@gmail.com>
2023-12-23 21:31:21 +0200

    add small fix

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
 import queue
 import time
-
+import uuid
 
 request_queue = queue.Queue()
 
-def generate_request(number_request):
-    for i in range(number_request):
-        request_id = request_queue.qsize() + 1
-        new_request = f"request {request_id}"
-        request_queue.put(new_request)
-        print(new_request)
-        time.sleep(1)
-
+def generate_request():
+    request_id = str(uuid.uuid4())
+    new_request = f"request {request_id}"
+    request_queue.put(new_request)
+    print( f"\nNew request: {request_id}")
+    time.sleep(1)
 
 def process_request():
-    while True:
-      
-        if not request_queue.empty():
-            processed_request = request_queue.get()
-            print(f"Processing: {processed_request}")
-            time.sleep(2)
-        else:
-            print("No requests to process.")
-            break
-
+    if not request_queue.empty():
+        processed_request = request_queue.get()
+        print(f"\nProcessing...{processed_request}")
+        time.sleep(2)
+    else:
+        print("\nNo requests to process.")
 
 
 def main():
 
-    while True:
-        number_req = input("Enter a count of queue: ").strip()
-        if not number_req.isdigit():  
-            print("Queue is empty, exit the program")
-            break
-        generate_request(int(number_req))
-        process_request()
+    try:
+        while True:
+            generate_request()
+            
+            process_request()            
+
+    except KeyboardInterrupt:
+        print("\nProgram terminated by user.")
 
 if __name__ == "__main__":
     main()
